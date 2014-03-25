d6898a4d97d5838ab1fba97e98558429dfcdd02b
AKovtunov <1sasha.sasha1@gmail.com>
2014-03-26 01:13:06 +0200

    Tire suggester

--- tire_sample.rb
+++ tire_sample.rb
@@ -10,10 +10,10 @@ User.create! name: 'Alexandr Kovtunov'
 # puts search[:real].results.inspect
 # puts "Possible ones:"
 # puts search[:possible].results.inspect
-Benchmark.ips do |x|
-  x.report('simple search') { User.search({search_query:"Alex"}) }
-  x.report('full search') { User.simple_search({search_query:"Alex"}) }
-end
+# Benchmark.ips do |x|
+#   x.report('simple search') { User.search({search_query:"Alex"}) }
+#   x.report('full search') { User.simple_search({search_query:"Alex"}) }
+# end
 
 puts "Add some more names "
 User.create! name: 'Alex Bierbrauer'
@@ -21,18 +21,37 @@ User.create! name: 'Alexander Bierbrauer'
 User.create! name: 'Alexeus Bierbrauer'
 User.create! name: 'Alexandr Bierbrauer'
 
-Benchmark.ips do |x|
-  x.report('simple search') { User.search({search_query:"Alex"}) }
-  x.report('full search') { User.simple_search({search_query:"Alex"}) }
-end
+# Benchmark.ips do |x|
+#   x.report('simple search') { User.search({search_query:"Alex"}) }
+#   x.report('full search') { User.simple_search({search_query:"Alex"}) }
+# end
 
 puts "And even more names "
-User.create! name: 'Alex Somesecondname'
-User.create! name: 'Alexander Somesecondname'
-User.create! name: 'Alexeus Somesecondname'
-User.create! name: 'Alexandr Somesecondname'
+User.create! name: 'Alexan Somesecondname'
+User.create! name: 'Alexandre Somesecondname'
+User.create! name: 'Violett Somesecondname'
+User.create! name: 'Sedrick Somesecondname'
 
-Benchmark.ips do |x|
-  x.report('simple search') { User.search({search_query:"Alex"}) }
-  x.report('full search') { User.simple_search({search_query:"Alex"}) }
+# Benchmark.ips do |x|
+#   x.report('simple search') { User.search({search_query:"Alex"}) }
+#   x.report('full search') { User.simple_search({search_query:"Alex"}) }
+# end
+
+puts "And different ones "
+User.create! name: 'Marco Polo'
+User.create! name: 'Emma Watson'
+User.create! name: 'Leonardo Di Caprio'
+User.create! name: 'Colin mcRye'
+#https://github.com/karmi/retire/blob/master/examples/tire-dsl.rb suggest
+s = Tire.suggest('users') do
+
+  # Notice that for standalone API, the block method is `suggestion` rather than `suggest`:
+  #
+  suggestion :term_suggest do
+    text 'Alexandir Kovtuniv'
+    term :name, size: 3, sort: 'frequency'
+  end
+
 end
+
+puts "Available corrections: #{s.results.texts.join(', ')}"
