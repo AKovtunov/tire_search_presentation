de96b44327b691787a8c354d4e8888003a004b20
AKovtunov <1sasha.sasha1@gmail.com>
2014-03-27 01:28:56 +0200

    Added even more functions

--- tire_sample.rb
+++ tire_sample.rb
@@ -5,53 +5,63 @@ User.create! name: 'Alex Kovtunov'
 User.create! name: 'Alexander Kovtunov'
 User.create! name: 'Alexeus Kovtunov'
 User.create! name: 'Alexandr Kovtunov'
+User.create! name: 'Alex Bierbrauer'
+User.create! name: 'John Bierbrauer'
+User.create! name: 'Alexeus Bierbrauer'
+User.create! name: 'Alexandr Bierbrauer'
+User.create! name: 'Alexan Somesecondname'
+User.create! name: 'Alexandre Somesecondname'
+User.create! name: 'Violett Somesecondname'
+User.create! name: 'Sedrick Somesecondname'
+User.create! name: 'Marco Polo'
+User.create! name: 'Emma Watson'
+User.create! name: 'Leonardo Di Caprio'
+User.create! name: 'Colin mcRye'
+
+# Benchmark.ips do |x|
+#   x.report('simple search') { User.simple_search({search_query:"Alex"}) }
+#   x.report('full search') { User.search({search_query:"Alex"}) }
+#   x.report('simple suggestion') { User.simple_suggestions({search_query:"Alex"}) }
+#   x.report('full suggestion') { User.suggestions({search_query:"Alex"}) }
+#   x.report('simple_search_with_suggestions'){ User.simple_search_with_suggestions({search_query:"Alex"}) }
+# end
+
+Benchmark.bm do |x|
+  x.report('simple search') { User.simple_search({search_query:"Alex"}) }
+  x.report('full search') { User.search({search_query:"Alex"}) }
+  x.report('simple suggestion') { User.simple_suggestions({search_query:"Alex"}) }
+  x.report('full suggestion') { User.suggestions({search_query:"Alex"}) }
+  x.report('simple_search_with_suggestions'){ User.simple_search_with_suggestions({search_query:"Alex"}) }
+end
+
 # search = User.search({search_query:"alexandar koftunov"})
 # puts "Real ones:"
 # puts search[:real].results.inspect
 # puts "Possible ones:"
 # puts search[:possible].results.inspect
-# Benchmark.ips do |x|
-#   x.report('simple search') { User.search({search_query:"Alex"}) }
-#   x.report('full search') { User.simple_search({search_query:"Alex"}) }
-# end
 
-puts "Add some more names "
-User.create! name: 'Alex Bierbrauer'
-User.create! name: 'Alexander Bierbrauer'
-User.create! name: 'Alexeus Bierbrauer'
-User.create! name: 'Alexandr Bierbrauer'
-
-# Benchmark.ips do |x|
-#   x.report('simple search') { User.search({search_query:"Alex"}) }
-#   x.report('full search') { User.simple_search({search_query:"Alex"}) }
-# end
-
-puts "And even more names "
-User.create! name: 'Alexan Somesecondname'
-User.create! name: 'Alexandre Somesecondname'
-User.create! name: 'Violett Somesecondname'
-User.create! name: 'Sedrick Somesecondname'
-
-# Benchmark.ips do |x|
-#   x.report('simple search') { User.search({search_query:"Alex"}) }
-#   x.report('full search') { User.simple_search({search_query:"Alex"}) }
-# end
-
-puts "And different ones "
-User.create! name: 'Marco Polo'
-User.create! name: 'Emma Watson'
-User.create! name: 'Leonardo Di Caprio'
-User.create! name: 'Colin mcRye'
-#https://github.com/karmi/retire/blob/master/examples/tire-dsl.rb suggest
-s = Tire.suggest('users') do
-
-  # Notice that for standalone API, the block method is `suggestion` rather than `suggest`:
-  #
-  suggestion :term_suggest do
-    text 'Alexandir Kovtuniv'
-    term :name, size: 3, sort: 'frequency'
-  end
-
+search = User.simple_search_with_suggestions({search_query:"Alexandir"})
+puts "Search returned:\n"
+search.results.each do |result|
+  puts result.inspect
 end
+search.suggestions.each do |name, options|
+  puts "Suggestion returned for #{name}:\n"
+  options.each do |option|
+    puts "* Raw result: #{option}"
+    option['options'].each do |option|
+      puts "* Possible name: #{option['text']} with P=(#{option['score']})"
+    end 
+  end
+end
+# suggestions = User.suggestions({search_query:"Alex"})
+# suggestions.results.suggestions.each do |name, options|
+#   puts "Suggestion returned for #{name}:\n"
+#   options.each do |option|
+#     puts "* Raw result: #{option}"
+#   end
+# end
 
-puts "Available corrections: #{s.results.texts.join(', ')}"
+
+# suggestions = User.simple_suggestions({search_query:"Alex"})
+# puts "Available corrections: #{suggestions.results.texts.join(', ')}"
--- tire_user_model.rb
+++ tire_user_model.rb
@@ -47,4 +47,72 @@ class User < ActiveRecord::Base;
     end
   end
 
+  #https://github.com/karmi/retire/blob/master/examples/tire-dsl.rb suggest
+  def self.suggestions(params)
+    Tire.search 'users' do
+
+      # To define a suggest using the term suggester, first provide a custom name for the suggest.
+      #
+      suggest :suggest_name do
+        # Specify the input text.
+        #
+        text params[:search_query]
+        # Then, define the field you want to use for suggestions and any options.
+        #
+        term :name, size: 3, sort: 'frequency'
+      end
+
+      # To define a suggest using the `phrase` suggest, use a different name.
+      suggest :phrase_suggest_name do
+        # Specify the text input text.
+        #
+        text params[:search_query]
+        # Again, define the field you want to use for suggestions and any options.
+        #
+        phrase :name, size: 3 do
+          # Optinally, configure the `smoothing` option...
+          #
+          smoothing :stupid_backoff, discount: 0.5
+
+          # ...or the `generator` option.
+          generator :name, min_word_len: 1
+        end
+      end
+    end
+  end
+
+# The results will be available in the `suggestions` property (which is iterable)
+#
+
+
+  def self.simple_suggestions(params)
+    Tire.suggest('users') do
+      # Notice that for standalone API, the block method is `suggestion` rather than `suggest`:
+      #
+      suggestion :term_suggest do
+        text params[:search_query]
+        term :name, size: 3, sort: 'frequency'
+      end
+
+    end
+  end
+
+  def self.simple_search_with_suggestions(params)
+    original_query = params[:search_query].tr('^A-Za-z0-9 ', '')
+    params[:search_query] = original_query.split(" ").map { |a| a<<("*") }.join(" ")
+    tire.search(load: true) do
+      query { string params[:search_query], default_operator: "AND" } if params[:search_query].present?
+      sort do
+        by :name
+      end
+      suggest :phrase_suggest_name do
+        text original_query
+        phrase :name, size: 3 do
+          smoothing :stupid_backoff, discount: 0.5
+          generator :name, min_word_len: 1
+        end
+      end
+    end
+  end
+
 end
